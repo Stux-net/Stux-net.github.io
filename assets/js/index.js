93047b99600bb33a270fd17334dc62f5261231d9
Stux-net <72707130+Stux-net@users.noreply.github.com>
2021-04-09 01:07:10 +0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,3 +32,52 @@ btn_twitter.addEventListener("click", function (e) {
 e.preventDefault();
   alert("I don't use Twitter , but you can follow me on Instagram 🖤");
 });
+
+// scroll top /////////////////////////
+
+let offset = 100;
+
+const scrollUp = document.querySelector(".scroll-up");
+const scrollUpSvgPath = document.querySelector(".scroll-up-svg-path");
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = "stroke-dashoffset 20ms";
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// update Dashoffset
+
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - (getTop() * pathLength) / height;
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+// on Scroll
+window.addEventListener("scroll", function () {
+  if (document.documentElement.clientWidth < 768) {
+    offset = 300;
+  }
+  if (document.documentElement.clientWidth < 670) {
+    offset = 400;
+  }
+  updateDashoffset();
+  if (getTop() > offset) {
+    scrollUp.classList.add("scroll-up-active");
+  } else {
+    scrollUp.classList.remove("scroll-up-active");
+  }
+});
+
+// active
+
+scrollUp.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+
+
